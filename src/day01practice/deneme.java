4f1efa745162afbf5c74b31744c6808236877de4
mustafakork <mstfbsrysrykb60@gmail.com>
2023-04-27 10:22:02 +0300

    deneme2

--- src/day01practice/deneme.java
+++ src/day01practice/deneme.java
@@ -2,7 +2,11 @@ package day01practice;
 
 public class deneme {
     public static void main(String[] args) {
+        int a=10;
 
     }
 }
 //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
+// hocam yapıstırınca gorunmuyor ctrl v deyip enrere basin
+///sifrenizi ongrenin diger turlü bagalmayi dneyelim
+//ok hocam bende bir deneyeyi
